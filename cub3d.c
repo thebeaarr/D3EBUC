57f493e0020ade28c795441a6485335f91c5e364
Mohamed Lakhdar <mlakhdar@c4r8p9.1337.ma>
2025-07-31 06:46:09 +0100

    ownedbyhunter

--- cub3d.c
+++ cub3d.c
@@ -6,7 +6,7 @@
 /*   By: mlakhdar <mlakhdar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/07/30 19:31:45 by mlakhdar          #+#    #+#             */
-/*   Updated: 2025/07/31 05:12:46 by mlakhdar         ###   ########.fr       */
+/*   Updated: 2025/07/31 06:45:42 by mlakhdar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -88,29 +88,61 @@ bool is_txt(char *path)
 {
   return (!strncmp(path, "NO", 2) || !strncmp(path, "WE", 2) || !strncmp(path, "EA", 2) || !strncmp(path, "SO", 2));
 }
+
+bool valid_path_xpm(char *path)
+{
+  char *xpm = strrchr(path, '.');
+  if(strncmp(xpm , ".xpm" , 4 ) ==0)
+    return true;
+  return false;
+}
+bool dup_dir(t_textures *t , char *dir )
+{
+  if(t->ea && strncmp(dir , "EA" , 2) == 0)
+    return false;
+  else if(t->no && strncmp(dir , "NO" , 2) == 0)
+    return false;
+  else if(t->so && strncmp(dir , "SO" , 2) == 0)
+    return false;
+  else if(t->we && strncmp(dir , "WE" ,2) == 0)
+    return false;
+  return true;
+}
 bool get_txt_path(t_textures **tx , char *txt)
 {
   t_textures *x = *tx;
   char *path = strchr(txt, '.');
+  if(dup_dir(x ,path))
+  {
+    write(2, path, 2);
+    return false;
+  }
+  if(!valid_path_xpm(path))
+  {
+    printf("not valid path xpm!\n");
+    return false;
+  }
   if(strncmp(txt , "NO" , 2) == 0)
-    x->no = path;
+    x->no = strdup(path);
   else if(strncmp(txt , "WE" , 2) == 0)
-    x->we = path;
+    x->we = strdup(path);
   else if(strncmp(txt, "SO" , 2) == 0)
-    x->so = path;
+    x->so = strdup(path);
   else if(strncmp(txt, "EA", 2) == 0)
-    x->ea = path;
+    x->ea = strdup(path);
   else
     return false;
   return true;
 }
+
 bool all_txt_set(t_textures *t)
 {
   return (t->ea && t->no && t->so && t->we);
 }
 
-t_textures *get_path_txts(t_lstfile *lst)
+bool get_path_txts(t_lstfile **lst)
 {
+  t_lstfile *l = *lst;
   t_textures *t;
   t = malloc(sizeof(t_textures));
   t_file *tmp;
@@ -118,32 +150,32 @@ t_textures *get_path_txts(t_lstfile *lst)
   t->no = NULL;
   t->so = NULL;
   t->we = NULL;
-  tmp = lst->head;
+  tmp = l->head;
   while(tmp)
   {
     if(is_txt(tmp->line))
     { 
         if(!get_txt_path(&t, tmp->line))
-          exit(1);
-        if(all_txt_set(t))
+          return false;
+        if (all_txt_set(t))
           break;
     }
     tmp = tmp->next;
   }
-  return t;
+  if(!all_txt_set(t))
+    return false;
+  l->txts = t;
+  return true;
 }
 
 bool valid_file(t_lstfile *lst)
 {
   t_file *(file);
   file = lst->head;
-  // gonna check the textures are they exist ? 
-  // checking for the color ( RGB ( C AND F )) are they exist and valid
-  // check if the maps is valid (important part i guess )
-  // first get the path of the textures
-  lst->txts = get_path_txts(lst);
-  
-  return true;
+  bool r;
+  r = false;
+  r = get_path_txts(&lst);
+  return r;
 }
 
 int main(int ac, char **av)
@@ -160,10 +192,13 @@ int main(int ac, char **av)
     }
     t_lstfile *lst = r_file(fd);
     close(fd);
-    print_cfile(lst);
+    // print_cfile(lst);
     if (valid_file(lst) == false)
+    {
+      printf("failed :(\n");
       return 1;
-    printf("rak ghadi blan \n");
+    }
+    printf("\nsuccessed to run the cube 3d.!!!\n");
   }
   else 
   {
