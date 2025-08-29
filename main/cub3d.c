742f4d33c6003bccee36e95072a1cc4c93fc7a0e
feeedback <m0h4m3dl4khd4r@gmail.com>
2025-08-29 19:51:15 +0100

    dd some files for mlx init sf

--- main/cub3d.c
+++ main/cub3d.c
@@ -1,6 +1,22 @@
 #include "../cub3d_parser/include/parser.h"
 
 
+int handle_close()
+{
+  printf("tfi daw\n");
+  exit(1);
+}
+
+int handle_key(int a )
+{
+  if(a == 65307)
+  {
+    printf("good bye!");
+    exit(0);
+  }
+  return 0;
+}
+
 int main(int ac , char **av)
 {
   if(ac == 2)
@@ -11,7 +27,8 @@ int main(int ac , char **av)
     // t_data { cub3d  , mlx min img , textures } ...
     void *mlx = mlx_init();
     void *win = mlx_new_window(mlx, 1000, 500, "medlkhdr");
-    (void)win;
+    mlx_hook(win , 17 , 0 ,handle_close ,NULL );
+    mlx_key_hook(win , handle_key , NULL);
     mlx_loop(mlx);
   }
   else
